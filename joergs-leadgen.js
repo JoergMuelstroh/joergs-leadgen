d2547ee4d2b4b5ddef1bfaea797ac7f65f736598
JoergMuelstroh <info@muelstroh.com>
2026-09-13 13:30:05 +0200

    Update joergs-leadgen.js

--- joergs-leadgen.js
+++ joergs-leadgen.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
